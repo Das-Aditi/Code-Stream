ebc69526f652aea2eb176981a1e3327da86aed4c
Aditi Das <aditidas3081@gmail.com>
2026-01-26 23:28:15 +0530

    Added details to Development Docs

--- Backend/fastapi/db.py
+++ Backend/fastapi/db.py
@@ -4,4 +4,4 @@ from config import MONGO_URL, DB_NAME
 client = MongoClient(MONGO_URL)
 db = client[DB_NAME]
 
-problem_collection = db["problems"]   # CONNECTS directly to Compass collection called "problem"
+problem_collection = db["problems"]   # CONNECTS directly to collection called "problem"
